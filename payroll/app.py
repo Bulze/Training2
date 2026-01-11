e306ab4a88c6491c077b1e4b88259ed7c059fdd4
Ognjen Markovic <ognjenmarkovic014@gmail.com>
2026-01-11 10:59:21 +0100

    Make payroll Flask bind PORT

--- payroll/app.py
+++ payroll/app.py
@@ -943,4 +943,6 @@ def compare_users():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", "5000"))
+    debug = os.getenv("FLASK_DEBUG", "").lower() in ("1", "true", "yes")
+    app.run(host="0.0.0.0", port=port, debug=debug)
